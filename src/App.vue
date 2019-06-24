5d4ebc5bf9eb7a8410b28ce251ebc1b31a397ff3
Javier Collipal <jcollipal1212@gmail.com>
2019-06-24 13:39:10 -0400

    cambio menor

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,20 @@
         <code v-text="'<el-button>'"></code>
         below
       </p>
+      <p>
+        Este proyecto esta listo para usar element UI y jest esta configurado para testear componentes
+      </p>
       <el-button>el-button</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
